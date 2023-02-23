8fa69831bef4f0ac1464b734025a3e4e484e8ea1
Enaturale <enaturaleb@gmail.com>
2023-02-23 16:42:43 +0100

    What is going on

--- backend/index.js
+++ backend/index.js
@@ -32,12 +32,57 @@ app.use(cors());
 
 const socketIO = require('socket.io')(http, {
     cors: {
-        origin: "<http://192.168.100.27:3000>"
+        origin: "http://192.168.100.27:3000"
     }
 });
 
+//generates rando string as the ID 
+const generateTD = () => Math.random().toString(36).substring(2, 10);
+
+let chatRooms =[
+
+]
+
 socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
+
+    //socket connection to chat room
+    socket.on("CreateRoom", (roomName) => {
+        socket.join(roomName);
+        //this willadd the new group name to the chat rooms array above
+        chatRooms.unshift({id: generateTD(), roomName, messages:[]})
+        //this will return the updated chat rooms via another event
+        socket.emit("roomsList", chatRooms);
+    })
+
+    //listener to find the created room
+    socket.on("findRoom", (id) => {
+        let result = chatRooms.filter((room) => room.id ==id);
+        socket.emit("foundRoom", result[0].messages);
+    })
+
+    //listener for new messages
+    socket.on("newMessage", (data) => {
+        const {room_id, message, user,timestamp} = data
+        let result = chatRooms.filter((room) =>  room_id == room_id);
+
+        const newMessage ={
+            id: generateTD(),
+            text: message,
+            user,
+            time: `${timestamp.hour}:${timestamp.mins}`,
+        };
+
+        //updates the chatroom messages
+        socket.io(result[0].name).emit("roomMessage", newMessage);
+        result[0].messages.push(newMessage);
+
+        //triggrt te events to reflet the new changes
+        socket.emit("roomsList", chatRooms);
+        socket.emit("foundRoom", result[0].messages)
+
+    })
+
 
     socket.on('disconnect', () => {
       socket.disconnect()
@@ -46,9 +91,7 @@ socketIO.on('connection', (socket) => {
 });
 
 app.get("/api", (req, res) => {
-    res.json({
-        message: "Hello world",
-    });
+    res.json(chatRooms);
 });
 
 http.listen(PORT, () => {
